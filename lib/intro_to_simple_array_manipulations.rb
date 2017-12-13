c183ea21d93c0ba7bcec7f0db58ab85f09b75a6c
apocalypse575 <apocalypse575@gmail.com>
2017-12-13 12:20:34 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_flatten(array)
+  return array.flatten
+end
+
+
+
+
+# # #using_flatten takes in an argument of an array that contains other arrays and uses t
+# he flatten method to return an array of strings
+
+# # #using_delete takes in two arguments, an array and a string, and uses the delete meth
+# od to remove any items from the array that are equal to that string
+
+# # #using_delete_at takes in two arguments, an array and an integer and deletes the elem
+# ent at the index of the array that is equal to that integer
